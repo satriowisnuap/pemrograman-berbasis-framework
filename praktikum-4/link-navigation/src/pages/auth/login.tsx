f7e31c007cb0652be24d54c9d4e512d6a3147823
satriowisnu <satriowisnuap@email.com>
2026-03-01 22:00:46 +0700

    Jobsheet 4 (Langkah 7 – Navigasi Imperatif (router.push))

--- praktikum-4/link-navigation/src/pages/auth/login.tsx
+++ praktikum-4/link-navigation/src/pages/auth/login.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const halamanLogin = () => {
-  
+  const { push } = useRouter();
+  const handlerLogin = () => {
+    // logic login disini
+    push("/produk");
+  };
   return (
     <div>
       <h1>Halaman Login</h1>
+      <button onClick={handlerLogin}>Login</button>
+      <br />
+      <button onClick={() => push("/produk")}>Login 2</button>
+      <br />
+      <button onClick={() => handlerLogin()}>login 3</button>
       <br />
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
