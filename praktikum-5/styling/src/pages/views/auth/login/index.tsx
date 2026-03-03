e5d1f70a91a9cc9c973e4fb8bb33f003b2785758
satriowisnu <satriowisnuap@email.com>
2026-03-03 20:56:00 +0700

    Jobsheet 5 Langkah 6 Inline Styling (CSS-in-JS)

--- praktikum-5/styling/src/pages/views/auth/login/index.tsx
+++ praktikum-5/styling/src/pages/views/auth/login/index.tsx
@@ -12,10 +12,14 @@ const TampilanLogin = () => {
     <div className={styles.login}>
       <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
       <button onClick={()=> handleLogin()}>Login</button> <br />
-      <h1 style={{color:"red",border:"1px solid red",borderRadius:"5px",padding:"5px"}}> belum punya akun</h1>
+      <h1 style={{color:"red",
+        border:"1px solid red",
+        borderRadius:"5px",
+        padding:"5px"}}> belum punya akun</h1>
       <Link href={"/auth/register"}>Ke Halaman Register</Link>
     </div>
   );
 }   
 
 export default TampilanLogin
+
