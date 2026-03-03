0c76afb5c9289cf530d554b503a7f5f9dac525a5
satriowisnu <satriowisnuap@email.com>
2026-03-03 20:37:20 +0700

    Jobsheet 5 Langkah 4 Conditional Rendering Navbar (Tanpa Navbar di Login)

--- praktikum-5/styling/src/components/layouts/Appshell/index.tsx
+++ praktikum-5/styling/src/components/layouts/Appshell/index.tsx
@@ -1,21 +1,24 @@
+import { useRouter} from "next/router";
 import Navbar from "../navbar";
 import Footer from "../footer";
+
+const diableNavbar = ["/auth/login", "/auth/register"];
 
 type AppShellProps = {
     children: React.ReactNode;
 }
 
 const AppShell = (props: AppShellProps) => {
+
     const { children } = props;
+    const { pathname } = useRouter();
 
     return (
-        <div className="wrapper">
-            <Navbar />
-            <main className="content">
-                {children}
-            </main>
-            <Footer />
-        </div>
+       <main>
+        { !diableNavbar.includes(pathname) && <Navbar /> }
+        {children}
+        <Footer />
+       </main>
     );
 };
 
