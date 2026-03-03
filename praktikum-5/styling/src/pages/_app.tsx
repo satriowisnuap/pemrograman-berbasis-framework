80e83de37f081526e6f90b3f46104a5bbc0b4b6e
satriowisnu <satriowisnuap@email.com>
2026-03-03 18:42:45 +0700

    Jobsheet 5 Langkah 1 Global CSS (Bagian b (Import Global CSS))

--- praktikum-5/styling/src/pages/_app.tsx
+++ praktikum-5/styling/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-// import Navbar from '@/components/layouts/navbar'
 import AppShell from '@/components/layouts/Appshell'
+// import Navbar from '@/components/layouts/navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
