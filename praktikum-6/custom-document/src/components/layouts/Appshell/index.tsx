de42df8cf412fad072a3c231f43570c47668ab10
satriowisnu <satriowisnuap@email.com>
2026-03-09 19:20:49 +0700

    Jobsheet 6 Tugas Praktikum 2

--- praktikum-6/custom-document/src/components/layouts/Appshell/index.tsx
+++ praktikum-6/custom-document/src/components/layouts/Appshell/index.tsx
@@ -1,25 +1,26 @@
-import { useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Navbar from "../navbar";
 import Footer from "../footer";
 
-const diableNavbar = ["/auth/login", "/auth/register"];
+// Menambahkan 404 disini
+const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 
 type AppShellProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 const AppShell = (props: AppShellProps) => {
+  const { children } = props;
+  const { pathname } = useRouter();
 
-    const { children } = props;
-    const { pathname } = useRouter();
-
-    return (
-       <main>
-        { !diableNavbar.includes(pathname) && <Navbar /> }
-        {children}
-        <Footer />
-       </main>
-    );
+  return (
+    <main>
+      {/* Navbar hanya tampil jika pathname tidak ada di disableNavbar */}
+      {!disableNavbar.includes(pathname) && <Navbar />}
+      {children}
+      <Footer />
+    </main>
+  );
 };
 
 export default AppShell;
