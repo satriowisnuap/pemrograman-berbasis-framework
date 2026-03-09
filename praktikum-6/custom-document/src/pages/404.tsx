fb661c51a712ec58c50dd7d75cb0bbd3109dc19e
satriowisnu <satriowisnuap@email.com>
2026-03-09 18:59:54 +0700

    Jobsheet 6 Langkah 6 Menampilkan Gambar dari Folder Public

--- praktikum-6/custom-document/src/pages/404.tsx
+++ praktikum-6/custom-document/src/pages/404.tsx
@@ -3,6 +3,7 @@ import style from "@/styles/404.module.scss";
 const Custom404 = () => {
   return (
     <div className={style.error}>
+      <img src="/404.png" alt="404 Error" className={style.error__image} />
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
     </div>
