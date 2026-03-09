eba53ad8fc306489f21afe72fac510b264fb4799
satriowisnu <satriowisnuap@email.com>
2026-03-09 19:18:17 +0700

    Jobsheet 6 Tugas Praktikum 1

--- praktikum-6/custom-document/src/pages/404.tsx
+++ praktikum-6/custom-document/src/pages/404.tsx
@@ -1,9 +1,22 @@
+import Head from "next/head";
 import style from "@/styles/404.module.scss";
 
 const Custom404 = () => {
   return (
     <div className={style.error}>
+      <Head>
+        {/* Judul Halaman */}
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta
+          name="description"
+          content="Halaman yang Anda cari tidak ditemukan"
+        />
+      </Head>
+
+      {/* Gambar ilustrasi 404 */}
       <img src="/404.png" alt="404 Error" className={style.error__image} />
+
+      {/* Deskripsi singkat tentang error 404 */}
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
     </div>
