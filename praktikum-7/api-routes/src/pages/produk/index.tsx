f609a75febfe0885a55885c004cae317f394f4e2
satriowisnu <satriowisnuap@email.com>
2026-03-09 22:45:22 +0700

    Jobsheet 7 Langkah 3 Fetch Data API di Frontend

--- praktikum-7/api-routes/src/pages/produk/index.tsx
+++ praktikum-7/api-routes/src/pages/produk/index.tsx
@@ -1,9 +1,47 @@
-import TampilanProduk from "../views/produk";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const Produk = () => {
-    return (
-        <TampilanProduk />
-    );
+type ProductType = {
+  id: string;
+  nama: string;
+  harga: number;
+  ukuran: string;
+};
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  //   },[]);
+
+  useEffect(() => {
+    fetch("/api/produk")
+      .then((response) => response.json())
+      .then((responsedata) => {
+        setProducts(responsedata.data);
+        // console.log("Data produk:", responsedata.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching produk:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Daftar Produk</h1>
+      {products.map((products: ProductType) => (
+        <div key={products.id}>
+          <h2>{products.nama}</h2>
+          <p>Harga: {products.harga}</p>
+          <p>Ukuran: {products.ukuran}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Produk;
+export default kategori;
