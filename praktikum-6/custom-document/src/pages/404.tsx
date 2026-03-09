9af8eb1905c3b9641948aa39014eb512bf115f9b
satriowisnu <satriowisnuap@email.com>
2026-03-09 18:54:20 +0700

    Jobsheet 6 Langkah 5 Styling Halaman 404

--- praktikum-6/custom-document/src/pages/404.tsx
+++ praktikum-6/custom-document/src/pages/404.tsx
@@ -1,10 +1,12 @@
+import style from "@/styles/404.module.scss";
+
 const Custom404 = () => {
   return (
-    <div>
+    <div className={style.error}>
       <h1>404 - Halaman Tidak Ditemukan</h1>
       <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Custom404;
