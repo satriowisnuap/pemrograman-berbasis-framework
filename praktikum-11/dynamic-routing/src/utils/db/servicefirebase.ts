bf7a4c0b9257dbc07462276312c713069a1537fc
satriowisnu <satriowisnuap@email.com>
2026-03-25 20:57:10 +0700

    Jobsheet 11 Bagian 2 – Implementasi CSR (Client Side Rendering)

--- praktikum-11/dynamic-routing/src/utils/db/servicefirebase.ts
+++ praktikum-11/dynamic-routing/src/utils/db/servicefirebase.ts
@@ -1,4 +1,11 @@
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 import app from "./firebase";
 
 const db = getFirestore(app);
@@ -9,5 +16,11 @@ export async function retrieveProducts(collectionName: string) {
     id: doc.id,
     ...doc.data(),
   }));
+  return data;
+}
+
+export async function retrieveProductById(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
   return data;
 }
