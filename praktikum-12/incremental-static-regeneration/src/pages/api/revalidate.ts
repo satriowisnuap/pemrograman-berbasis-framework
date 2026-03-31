54b91643458af213980f13cfe411b766ea9db80d
satriowisnu <satriowisnuap@email.com>
2026-04-01 05:26:52 +0700

    Jobsheet 12 (D. On-Demand Revalidation ) Bagian 2 – Menambahkan Parameter Data

--- praktikum-12/incremental-static-regeneration/src/pages/api/revalidate.ts
+++ praktikum-12/incremental-static-regeneration/src/pages/api/revalidate.ts
@@ -3,17 +3,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   revalidated: boolean;
+  message?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  try {
-    await res.revalidate("/produk/static");
-    return res.status(200).json({ revalidated: true });
-  } catch (error) {
-    console.error("Error in API route:", error);
-    res.status(500).send({ revalidated: false });
+  if (req.query.token !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({
+      revalidated: false,
+      message: "Insert correct token",
+    });
   }
+
+  if (req.query.data === "produk") {
+    try {
+      await res.revalidate("/produk/static");
+      return res.status(200).json({ revalidated: true });
+    } catch (error) {
+      console.error("Error in API route:", error);
+      res.status(500).send({ revalidated: false });
+    }
+  }
+
+  return res.json({
+    revalidated: false,
+    message: "Invalid query parameter. Expected 'data=produk'.",
+  });
 }
