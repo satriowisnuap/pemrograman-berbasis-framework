099e59edce23f0728a5ad675bb84c75e38883b0d
satriowisnu <satriowisnuap@email.com>
2026-04-01 05:12:34 +0700

    Jobsheet 12 Bagian 2 – Pengujian ISR

--- praktikum-12/incremental-static-regeneration/src/pages/produk/[produk].tsx
+++ praktikum-12/incremental-static-regeneration/src/pages/produk/[produk].tsx
@@ -8,23 +8,23 @@ const HalamanProduk = ({ product }: { product: ProductType }) => {
   {
     /Digunakan client-side rendering/;
   }
-  const { query } = useRouter();
-  const { data, error, isLoading } = useSWR(
-    `/api/produk/${query.produk}`,
-    fetcher,
-  );
-
-  return (
-    <div>
-      <DetailProduk products={isLoading ? [] : data.data} />
-    </div>
-  );
+  // const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/produk/${query.produk}`,
+  //   fetcher,
+  // );
 
   // return (
   //   <div>
-  //     <DetailProduk products={product} />
+  //     <DetailProduk products={isLoading ? [] : data.data} />
   //   </div>
   // );
+
+  return (
+    <div>
+      <DetailProduk products={product} />
+    </div>
+  );
 };
 
 export default HalamanProduk;
@@ -49,25 +49,25 @@ export default HalamanProduk;
 {
   /Digunakan static-side generation/;
 }
-// export async function getStaticPaths() {
-//   const res = await fetch(`http://localhost:3000/api/produk`);
-//   const respone = await res.json();
+export async function getStaticPaths() {
+  const res = await fetch(`http://localhost:3000/api/produk`);
+  const respone = await res.json();
 
-//   const paths = respone.data.map((product: ProductType) => ({
-//     params: { produk: product.id },
-//   }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
+  const paths = respone.data.map((product: ProductType) => ({
+    params: { produk: product.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
-// export async function getStaticProps({ params }: any) {
-//   const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-//   const respone: { data: ProductType[] } = await res.json();
-//   return {
-//     props: {
-//       product: respone.data,
-//     },
-//   };
-// }
+export async function getStaticProps({ params }: any) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const respone: { data: ProductType[] } = await res.json();
+  return {
+    props: {
+      product: respone.data,
+    },
+  };
+}
