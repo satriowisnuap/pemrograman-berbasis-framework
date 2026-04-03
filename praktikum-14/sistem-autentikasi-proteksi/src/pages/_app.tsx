5c4d6b6f8a0440f26ef70db1f2742d64efeb6d27
satriowisnu <satriowisnuap@email.com>
2026-04-03 19:31:39 +0700

    Jobsheet 14 Bagian 4 – Tambahkan SessionProvider

--- praktikum-14/sistem-autentikasi-proteksi/src/pages/_app.tsx
+++ praktikum-14/sistem-autentikasi-proteksi/src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import AppShell from '@/components/layouts/Appshell'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import AppShell from "@/components/layouts/Appshell";
 // import Navbar from '@/components/layouts/navbar'
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
+      <SessionProvider session={pageProps.session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
     </div>
   );
-};
+}
