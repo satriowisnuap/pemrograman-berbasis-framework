eadf08e7911dc521bc18a87032297042407a8bad
satriowisnu <satriowisnuap@email.com>
2026-04-06 05:09:04 +0700

    Jobsheet 18 Optimasi Gambar Lokal (Public Folder)

---
+++ praktikum-18/Optimize/my-app/src/pages/editor/index.tsx
@@ -0,0 +1,17 @@
+const HalamanEditor = () => {
+  return (
+    <div>
+      <div className="admin">
+        <h1>Halaman Editor</h1>
+        <p>
+          Selamat datang di halaman editor! Anda memiliki akses untuk mengelola
+          konten dan membuat perubahan pada aplikasi ini. Di sini, Anda dapat
+          menambah, mengedit, dan menghapus konten sesuai dengan tanggung jawab
+          Anda.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanEditor;
