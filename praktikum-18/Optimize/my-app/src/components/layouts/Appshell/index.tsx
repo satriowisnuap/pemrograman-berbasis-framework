3f1de8f1152efbae3cbb2498e41996d6b193889c
satriowisnu <satriowisnuap@email.com>
2026-04-06 07:20:36 +0700

    Jobsheet 18 Font Optimize

--- praktikum-18/Optimize/my-app/src/components/layouts/Appshell/index.tsx
+++ praktikum-18/Optimize/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,24 +1,29 @@
 import { useRouter } from "next/router";
 import Navbar from "../navbar";
-import Footer from "../footer";
+import { Roboto } from "next/font/google";
+// import dynamic from "next/dynamic";
 
-// Menambahkan 404 disini
+// const Navbar = dynamic(() => import("../navbar"));
+
 const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 
 type AppShellProps = {
   children: React.ReactNode;
 };
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
   const { pathname } = useRouter();
 
   return (
-    <main>
-      {/* Navbar hanya tampil jika pathname tidak ada di disableNavbar */}
+    <main className={roboto.className}>
       {!disableNavbar.includes(pathname) && <Navbar />}
       {children}
-      <Footer />
     </main>
   );
 };
